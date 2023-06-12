08ded217060ab05a3d227a96b7201f50ebcc74b7
Joaquin Mendez <joaquin.enmt@gmail.com>
2023-06-12 15:37:30 -0400

    Mas funcionalidades de js

--- js/app.js
+++ js/app.js
@@ -26,47 +26,55 @@ window.addEventListener('DOMContentLoaded', () => {
         [0,4,8],
         [2,4,6],
     ]
+
+    function ValidacionResultados() {
+        let roundWin = false;
+        for (let i = 0; i < 7; i++) {
+            const winCondicion = condition[i];
+            const a = board[winCondicion[0]];
+            const b = board[winCondicion[1]];
+            const c = board[winCondicion[2]];
+        
+            if (a === '' || b === '' || c === '') {
+                continue;
+            }
+            if (a===b && b===c) {
+                roundWin = true;
+                break;
+            }
+        }
+    
+        if (roundWin) {
+            anuncio(currentPl === "X" ? PlayerO_Win : PlayerX_Win);
+            isGameActive=false;
+            return;
+        }
+        if (!board.includes("")) {
+            anuncio(Draw);
+        }
+    }
+    
+    const anuncio = (type) => {
+        switch (type) {
+            case PlayerO_Win:
+                anunciador.innerHTML = 'Jugador <span class="playerO">O</span> Gana';
+                break;
+        
+            case PlayerX_Win:
+                anunciador.innerHTML = 'Jugador <span class="playerX">X</span> Gana';
+                break;
+            case Draw:
+                anunciador.innerHTML = 'Empate';
+                break;
+        }
+        anunciador.classList.remove("hide");
+    }
+
+    const validAction = (tile) =>{
+        return tile.innerText !== "X" && tile.innerText !== "O";
+    };
+
+    const updateBoard = (index) =>{
+        board[index] = currentPl;
+    };
 });
-
-function ValidacionResultados() {
-    let roundWin = false;
-    for (let i = 0; i < 7; i++) {
-        const winCondicion = condition[i];
-        const a = board[winCondicion[0]];
-        const b = board[winCondicion[1]];
-        const c = board[winCondicion[2]];
-    
-        if (a === '' || b === '' || c === '') {
-            continue;
-        }
-        if (a===b && b===c) {
-            roundWin = true;
-            break;
-        }
-    }
-
-    if (roundWin) {
-        anuncio(currentPl === "X" ? PlayerO_Win : PlayerX_Win);
-        isGameActive=false;
-        return;
-    }
-    if (board.includes("")) {
-        anuncio(Draw);
-    }
-}
-
-const anuncio = (type) => {
-    switch (type) {
-        case PlayerO_Win:
-            anunciador.innerHTML = 'Jugador <span class="playerO">O</span> Gana';
-            break;
-    
-        case PlayerX_Win:
-            anunciador.innerHTML = 'Jugador <span class="playerX">X</span> Gana';
-            break;
-        case Draw:
-            anunciador.innerHTML = 'Empate';
-            break;
-    }
-    anunciador.classList.remove("hide");
-}
